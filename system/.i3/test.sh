10a7dccb96ecd56895a614369a54f6a3289462ca
Matthieu Coudron <mattator@gmail.com>
2015-04-22 12:18:02 +0200

    transfer.zsh is not sourced yet but once sourced, you can use 'transfer <file> to upload a file'

--- system/.i3/test.sh
+++ system/.i3/test.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
-i3-msg "workspace 9; append_layout /home/teto/.i3/ws1.json"
+i3-msg "workspace 9; append_layout $HOME/.i3/test.json"
+
+urxvt -cd $HOME/dce &
 
 
-urxvt -e sh -c "cd neovim;$SHELL"
-urxvt -e sh -c "$SHELL"
-urxvt -e sh -c "cd ns3off;$SHELL"
-urxvt -e sh -c "cd dce;$SHELL"
+#i3-msg "workspace 9; append_layout /home/teto/.i3/ws1.json"
+
+##urxvt -e sh -c "cd $HOME/ns3off;$SHELL" &
+#urxvt -cd $HOME/ns3off&
+
+#urxvt -e sh -c "cd $HOME/neovim;$SHELL"&
+#urxvt -e sh -c "$SHELL"&
+##urxvt -e sh -c "cd $HOME/dce;$SHELL" &
+#urxvt -name DCE -cd $HOME/dce&
