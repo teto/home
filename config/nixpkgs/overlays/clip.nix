dd7a142fa8a74c5a6513f29df4257debdbbfe02e
Matthieu Coudron <mattator@gmail.com>
2017-08-27 00:37:15 +0200

    Updating nix config

---
+++ config/nixpkgs/overlays/clip.nix
@@ -0,0 +1,9 @@
+self: super:
+{
+
+  # we override the default Clipboard packages for greenclip
+   Clipboard = super.haskellPackages.Clipboard.overrideAttrs (oldAttrs: {
+	  name = "Clipboard";
+	  src = ~/greenclip/lib/Clipboard;
+	});
+}
