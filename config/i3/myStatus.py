cb9944699a7cbc70df6a8c2f51c6b39303f761df
Matthieu Coudron <mattator@gmail.com>
2018-02-05 19:56:45 +0900

    default timezone for i3pystatus

--- config/i3/myStatus.py
+++ config/i3/myStatus.py
@@ -32,8 +32,9 @@ clock = status.register(
         # ("%a %-d Format 1",'Europe/Dublin'),
         # "%a %-d %b %X ",
         # 
-        (" %a %-d %X", 'Asia/Tokyo'),
-        (" %a %-d %X", 'Europe/Paris'),
+        " %a %-d %X",
+        (" %a %-d %X (Tokyo)", 'Asia/Tokyo'),
+        (" %a %-d %X (Paris)", 'Europe/Paris'),
     ],
     on_leftclick="termite -e ikhal",
     # on_leftclick="xmessage toto",
