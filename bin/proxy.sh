dd37b1d9647ef4b00af4b39b2826a2beac13f240
Matthieu Coudron <mattator@gmail.com>
2018-02-26 16:23:21 +0900

    added script to run proxy

---
+++ bin/proxy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# TODO put it in function /other file
+export http_proxy=http://proxy.iiji.jp:8080/
+export https_proxy=$http_proxy
+export ftp_proxy=$http_proxy
+export rsync_proxy=$http_proxy
+export no_proxy="localhost,127.0.0.1"
+
+
