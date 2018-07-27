298750d7ee8c49377380adc1b09eb71a7225ef97
Matthieu Coudron <mattator@gmail.com>
2018-07-28 02:36:52 +0900

    added home

--- nixpkgs/home-macbook.nix
+++ nixpkgs/home-macbook.nix
@@ -2,6 +2,15 @@
 {
   imports = [
     ./home-common.nix
+    ./home-desktop.nix
   ];
   # home.keyboard.layout = "fr,us";
+
+  programs.home-manager = {
+    enable = true;
+    # path = https://github.com/rycee/home-manager/archive/master.tar.gz;
+    # failshome.folder +
+    # must be a string
+    path =  "/home/teto/home-manager";
+  };
 }
