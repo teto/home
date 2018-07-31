610c0ec4beb8683400c34201b5ca708a9eaf39ce
Matthieu Coudron <mattator@gmail.com>
2018-07-31 12:25:15 +0900

    working on macbookkeymaps

--- nixpkgs/home-macbook.nix
+++ nixpkgs/home-macbook.nix
@@ -13,4 +13,17 @@
     # must be a string
     path =  "/home/teto/home-manager";
   };
+
+
+  # you can switch from cli with xkb-switch
+  # or xkblayout-state
+  # xev -event keyboard to retreive key symbols
+  # xmodmap will show the modifiers list
+  home.keyboard = {
+    # layout = "fr,us";
+    # options = [ "grp:caps_toggle" "grp_led:scroll" ];
+    # TODO can add Mod4 
+    options = [ "add Mod1 Super_L" ];
+    # options = [ "add Mod1 Alt_R" ];
+  };
 }
