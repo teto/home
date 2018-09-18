cfc1f26e3a1c449760faeeea590e10fe4a22534f
Matthieu Coudron <mattator@gmail.com>
2018-09-18 20:51:29 +0900

    updated notmuch tags and gImageReader

--- nixpkgs/overlays/pkgs/gImageReader/default.nix
+++ nixpkgs/overlays/pkgs/gImageReader/default.nix
@@ -1,47 +1,22 @@
-{ stdenv
-, fetchFromGitHub
-, cmake
-, pkgconfig
-, sane-backends # frontends
-, podofo
-, libjpeg
-, djvulibre
-, libxmlxx3
-, libzip
-, tesseract
-
-# , libsForQt5
-, withQt ? false
-# , poppler
-# , quazip
-, qtbase ? null
-# , qtsvg
-# , qtmultimedia
-# , qttools
-
+{ stdenv , fetchFromGitHub, cmake , pkgconfig, libuuid
+, sane-backends, podofo, libjpeg, djvulibre, libxmlxx3, libzip, tesseract
+, enchant, intltool, poppler, json-glib
 # needs to be python3
 , python
-, enchant
-, intltool
 
 # Gtk deps
-, withGtk ? true
-, gtk3 ? null
-, gtkmm ? null
-, pkgs
+, gtk3, gobjectIntrospection, wrapGAppsHook
+, gnome3, gtkspell3, gtkspellmm, cairomm
 }:
 
-assert withGtk -> !withQt  && gtk3 != null;
-assert withQt  -> !withGtk && qtbase  != null;
-
 let
-  variant = if withGtk then "gtk" else if withQt then "qt" else "cli";
+  variant = "gtk";
   pythonEnv = python.withPackages( ps: with ps;[ pygobject3 ] );
-
 in
 stdenv.mkDerivation rec {
   name = "gImageReader-${version}";
   version = "3.2.99";
+
   src = fetchFromGitHub {
     owner= "manisandro";
     repo = "gImageReader";
@@ -55,43 +30,33 @@ stdenv.mkDerivation rec {
     intltool
     libxmlxx3
     libzip
-    pkgs.libuuid
+    libuuid
     pkgconfig
     sane-backends
     podofo
     libjpeg
     djvulibre
-    pkgs.gobjectIntrospection 
     tesseract
-    pkgs.wrapGAppsHook
+    poppler.dev
   ] 
-  ++ stdenv.lib.optionals withGtk  ( with pkgs;[
+  # Gtk specific packets
+  ++ [
+
+    gobjectIntrospection 
     gnome3.gtkmm
     gtkspell3
     gtkspellmm
     gnome3.gtksourceview
     gnome3.gtksourceviewmm
     cairomm
-    poppler.dev
     json-glib
-    libxml2
-  ])
-  ++ stdenv.lib.optionals withQt [
-    # qtbase
-
-    # # qtspell # not packaged yetm same author
-    # # libsForQt5.
-    # poppler  # added 2015-12-19
-    # quazip
   ];
 
   # interface type can be where <type> is either gtk, qt5, qt4
-  # "-DCMAKE_INSTALL_PREFIX=$(out)"
   cmakeFlags = [  "-DINTERFACE_TYPE=${variant}" ];
-  # configurePhase = "cmake .";
 
   meta = with stdenv.lib; {
-    description = "A collaborative drawing program that allows multiple users to sketch on the same canvas simultaneously";
+    description = "A simple Gtk/Qt front-end to tesseract-ocr.";
     homepage = https://github.com/manisandro/gImageReader;
     license = licenses.gpl3;
     maintainers = with maintainers; [teto];
