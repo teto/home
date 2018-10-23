17ee720fbd121d3b6cb8c3faab05f8b0eea8e0c3
Matthieu Coudron <mattator@gmail.com>
2018-10-23 12:24:09 +0900

    Added prop_cycle to test in mptcpanalyzer
    plus some config for tor

--- nixpkgs/modules/tor.nix
+++ nixpkgs/modules/tor.nix
@@ -4,6 +4,16 @@
   services.tor = {
     enable = false;
     client.enable = true;
+    enableGeoIP = true;
+    dns.enable = false; # default false
+    relay.enable = false;
+
+    hiddenServices = { "my-hidden-service-example".map = [
+        { port = 22; }                # map ssh port to this machine's ssh
+        { port = 80; toPort = 8080; } # map http port to whatever runs on 8080
+        { port = "sip"; toHost = "mail.example.com"; toPort = "imap"; } # because we can
+      ];
+    };
   };
 }
 
