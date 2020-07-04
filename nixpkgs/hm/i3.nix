28a411e55312c6b215530b681c620e455084723a
Matthieu Coudron <mattator@gmail.com>
2020-07-04 18:45:14 +0200

    rebase ok

--- nixpkgs/hm/i3.nix
+++ nixpkgs/hm/i3.nix
@@ -370,7 +370,6 @@ in
 
   wayland.windowManager.sway = {
     enable = true;
-
     config = (removeAttrs  config.xsession.windowManager.i3.config ["startup" "bars"])
       // {
           input = {
