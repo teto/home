736106a85cbe1729b361469af35fe8646b55e8f2
Matthieu Coudron <mcoudron@hotmail.com>
2021-04-06 11:59:37 +0200

    added gh.nix module

---
+++ hm/profiles/gh.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, lib,  ... } @ args:
+let
+  secrets = import ./secrets.nix;
+in
+{
+  programs.gh = {
+    enable = true;
+    aliases = {
+       # n  gh alias set co --shell 'id="$(gh pr list -L100 | fzf | cut -f1)"; [ -n "$id" ] && gh pr checkout "$id"' 
+      co = ''--shell 'id="$(gh pr list -L100 | fzf | cut -f1)"; [ -n "$id" ] && gh pr checkout "$id"''';
+
+
+
+
+  }
+}
