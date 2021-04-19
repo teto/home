09c3fee27a20b6932fc1653e36128be92950f471
Matthieu Coudron <mcoudron@hotmail.com>
2021-04-19 19:37:54 +0200

    further configuration of the redis

--- nixos/modules/redis.nix
+++ nixos/modules/redis.nix
@@ -4,8 +4,11 @@
     enable = true;
     logLevel = "debug";
 
+    # default port is 6379
+    port = 4242;
     openFirewall = false;
-
+    # passwordFile = "./redis.txt";
+    requirePass = "toto";
     # settings = {};
   };
 }
