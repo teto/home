eb0bf9cb039b37c2a2ebaaab66fcfa91bd3f562a
Matthieu Coudron <mcoudron@hotmail.com>
2021-10-11 21:56:07 +0200

    reworked redis config

--- nixos/modules/redis.nix
+++ nixos/modules/redis.nix
@@ -19,14 +19,19 @@
       tls-port = 4242;
       tls-cert-file = "${../../data/server.crt}";
       tls-key-file = "${../../data/server.key}";
+
+      # disable client authentification
+      tls-auth-clients = "no";
+
       # Either tls-ca-cert-file or tls-ca-cert-dir must be specified when tls-cluster, tls-replication or tls-auth-clients are enabled!
       # Configure a CA certificate(s) bundle or directory to authenticate TLS/SSL
       # clients and peers.  Redis requires an explicit configuration of at least one
       # of these, and will not implicitly use the system wide configuration.
       #
       # tls-ca-cert-file ca.crt
-      tls-ca-cert-dir = "/etc/ssl/certs";
-      syslog-enabled = "yes";
+      # tls-ca-cert-dir = "/etc/ssl/certs";
+      tls-ca-cert-file = "${../../data/ca.crt}";
+      # syslog-enabled = "yes";
     };
   };
 }
