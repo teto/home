dcdcaaf4d1935e37295f4fe04042a59b2972c21e
Matthieu Coudron <mcoudron@hotmail.com>
2021-12-18 20:02:53 +0100

    fixed redis config

--- nixos/modules/redis.nix
+++ nixos/modules/redis.nix
@@ -1,57 +1,59 @@
 { config, lib, pkgs, ... }:
 {
   services.redis = {
-    enable = true;
-    logLevel = "debug";
 
-    # default port is 6379
-    port = 0;
-    openFirewall = false;
-    # passwordFile = "./redis.txt";
-    requirePass = "toto";
+    servers.default = {
+      enable = true;
+      logLevel = "debug";
+      # default port is 6379
+      port = 0;
+      openFirewall = false;
+      # passwordFile = "./redis.txt";
+      requirePass = "toto";
 
-    # https://redis.io/topics/config
-    # https://raw.githubusercontent.com/redis/redis/6.0/redis.conf
-    # to enable TLS
+      # https://redis.io/topics/config
+      # https://raw.githubusercontent.com/redis/redis/6.0/redis.conf
+      # to enable TLS
 
-    settings = {
-      # port = 0; # conflicts with module one
-      tls-port = 4242;
-      tls-cert-file = "${../../data/server.crt}";
-      tls-key-file = "${../../data/server.key}";
+      settings = {
+        # port = 0; # conflicts with module one
+        tls-port = 4242;
+        tls-cert-file = "${../../data/server.crt}";
+        tls-key-file = "${../../data/server.key}";
 
-      # disable client authentification
-      tls-auth-clients = "no";
+        # disable client authentification
+        tls-auth-clients = "no";
 
-      # Either tls-ca-cert-file or tls-ca-cert-dir must be specified when tls-cluster, tls-replication or tls-auth-clients are enabled!
-      # Configure a CA certificate(s) bundle or directory to authenticate TLS/SSL
-      # clients and peers.  Redis requires an explicit configuration of at least one
-      # of these, and will not implicitly use the system wide configuration.
-      #
-      # tls-ca-cert-file ca.crt
-      # tls-ca-cert-dir = "/etc/ssl/certs";
-      tls-ca-cert-file = "${../../data/ca.crt}";
-      # syslog-enabled = "yes";
+        # Either tls-ca-cert-file or tls-ca-cert-dir must be specified when tls-cluster, tls-replication or tls-auth-clients are enabled!
+        # Configure a CA certificate(s) bundle or directory to authenticate TLS/SSL
+        # clients and peers.  Redis requires an explicit configuration of at least one
+        # of these, and will not implicitly use the system wide configuration.
+        #
+        # tls-ca-cert-file ca.crt
+        # tls-ca-cert-dir = "/etc/ssl/certs";
+        tls-ca-cert-file = "${../../data/ca.crt}";
+        # syslog-enabled = "yes";
 
 
-# Configure allowed ciphers.  See the ciphers(1ssl) manpage for more information
-# about the syntax of this string.
-#
-# Note: this configuration applies only to <= TLSv1.2.
-#
-      tls-ciphers = "DEFAULT:!MEDIUM";
+  # Configure allowed ciphers.  See the ciphers(1ssl) manpage for more information
+  # about the syntax of this string.
+  #
+  # Note: this configuration applies only to <= TLSv1.2.
+  #
+        tls-ciphers = "DEFAULT:!MEDIUM";
 
-# Configure allowed TLSv1.3 ciphersuites.  See the ciphers(1ssl) manpage for more
-# information about the syntax of this string, and specifically for TLSv1.3
-# ciphersuites.
-#
-# tls-ciphersuites TLS_CHACHA20_POLY1305_SHA256
+  # Configure allowed TLSv1.3 ciphersuites.  See the ciphers(1ssl) manpage for more
+  # information about the syntax of this string, and specifically for TLSv1.3
+  # ciphersuites.
+  #
+  # tls-ciphersuites TLS_CHACHA20_POLY1305_SHA256
 
-# When choosing a cipher, use the server's preference instead of the client
-# preference. By default, the server follows the client's preference.
-#
-      tls-prefer-server-ciphers = "yes";
+  # When choosing a cipher, use the server's preference instead of the client
+  # preference. By default, the server follows the client's preference.
+  #
+        tls-prefer-server-ciphers = "yes";
 
+      };
     };
   };
 }
