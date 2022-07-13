cde599c594c3cdc89bffa2b992c0368d5521ef56
Matthieu Coudron <mcoudron@hotmail.com>
2022-07-13 10:49:59 +0200

    added postgresql-init.txt

--- nixos/profiles/postgresql.nix
+++ nixos/profiles/postgresql.nix
@@ -11,7 +11,9 @@
 
 	  # enableTCPIP = true; # if false, use TCP via localhost only or via socket
 
-	 # ensureUsers = 
+	 # ensureUsers =  [];
+	 # initdbArgs
+	 initialScript = ./postgresql-init.txt;
   };
 
   # see https://www.pgadmin.org/docs/pgadmin4/6.8/config_py.html
