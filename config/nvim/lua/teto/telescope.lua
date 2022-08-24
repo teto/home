ebb7b2d3fea67b74bd1d7e9fa998ee70e0750bf4
Matthieu Coudron <mcoudron@hotmail.com>
2022-08-24 17:24:17 +0200

    remove marks

--- config/nvim/lua/teto/telescope.lua
+++ config/nvim/lua/teto/telescope.lua
@@ -3,13 +3,18 @@ function M.telescope_create_keymaps()
 
 -- lua require('telescope.builtin').vim_options{}
 
-vim.keymap.set ('n', "<Leader>g", function () vim.cmd("FzfFiles") end)
-vim.keymap.set ('n', "<Leader>o", function () vim.cmd("FzfGitFiles") end)
-vim.keymap.set ('n', "<Leader>F", function () vim.cmd("FzfFiletypes") end)
-vim.keymap.set ('n', "<Leader>h", function () vim.cmd("FzfHistory") end)
+vim.keymap.set ('n', "<Leader>g", function () require'telescope.builtin'.find_files{} end )
+vim.keymap.set ('n', "<Leader>b", function () require'telescope.builtin'.buffers{} end )
+vim.keymap.set ('n', "<Leader>o", function () require'telescope.builtin'.git_files{} end )
+-- vim.keymap.set ('n', "<Leader>F", function () vim.cmd("FzfFiletypes") end)
 vim.keymap.set ('n', "<Leader>t", function () require'telescope.builtin'.tags{} end )
+vim.keymap.set ('n', "<Leader>l", function () require'telescope.builtin'.live_grep{} end )
+vim.keymap.set ('n', "<Leader>h", function () require'telescope.builtin'.oldfiles{} end )
+
+-- TODO search menus
+-- vim.keymap.set ('n', "<Leader>m", vim.notify("NOT IMPLEMENTED YET") )
 vim.keymap.set ('n', "<Leader>C",
-  function () require'telescope.builtin'.colorscheme{ enable_preview = true; } end 
+  function () require'telescope.builtin'.colorscheme{ enable_preview = true; } end
 )
 end
 
--- hm/profiles/neovim.nix
+++ hm/profiles/neovim.nix
@@ -6,6 +6,14 @@ let
 	config = lib.optionalString (attrs ? config)  (genBlockLua attrs.plugin.pname attrs.config);
   };
 
+  # pluginConfigLua = p:
+  #   if p ? plugin && (p.config or "") != "" then ''
+  #     -- ${p.plugin.pname} {{{
+  #     ${p.config}
+  #     -- }}}
+  #   '' else
+  #     "";
+
   genBlockLua = title: content: ''
       -- ${title} {{{
       ${content}
@@ -271,51 +279,51 @@ let
     #   require("lsp_lines").register_lsp_virtual_lines()
     #   '';
     # })
-    (luaPlugin {
-      plugin = marks-nvim;
-      config = ''
-      require'marks'.setup {
-        -- whether to map keybinds or not. default true
-        default_mappings = true,
-        -- which builtin marks to show. default {} but available:  ".", "<", ">", "^"
-        builtin_marks = {},
-        -- whether movements cycle back to the beginning/end of buffer. default true
-        cyclic = true,
-        -- whether the shada file is updated after modifying uppercase marks. default false
-        force_write_shada = false,
-        -- how often (in ms) to redraw signs/recompute mark positions.
-        -- higher values will have better performance but may cause visual lag,
-        -- while lower values may cause performance penalties. default 150.
-        refresh_interval = 250,
-        -- sign priorities for each type of mark - builtin marks, uppercase marks, lowercase
-        -- marks, and bookmarks.
-        -- can be either a table with all/none of the keys, or a single number, in which case
-        -- the priority applies to all marks.
-        -- default 10.
-        sign_priority = { lower=10, upper=15, builtin=8, bookmark=20 },
-        -- marks.nvim allows you to configure up to 10 bookmark groups, each with its own
-        -- sign/virttext. Bookmarks can be used to group together positions and quickly move
-        -- across multiple buffers. default sign is '!@#$%^&*()' (from 0 to 9), and
-        -- default virt_text is "".
-        bookmark_0 = {
-          sign = "⚑",
-          virt_text = "hello world"
-        },
-        mappings = {}
-    }
-    '';
-    })
 
+    # (luaPlugin {
+    #   plugin = marks-nvim;
+    #   config = ''
+    #   require'marks'.setup {
+    #     -- whether to map keybinds or not. default true
+    #     default_mappings = true,
+    #     -- which builtin marks to show. default {} but available:  ".", "<", ">", "^"
+    #     builtin_marks = {},
+    #     -- whether movements cycle back to the beginning/end of buffer. default true
+    #     cyclic = true,
+    #     -- whether the shada file is updated after modifying uppercase marks. default false
+    #     force_write_shada = false,
+    #     -- how often (in ms) to redraw signs/recompute mark positions.
+    #     -- higher values will have better performance but may cause visual lag,
+    #     -- while lower values may cause performance penalties. default 150.
+    #     refresh_interval = 250,
+    #     -- sign priorities for each type of mark - builtin marks, uppercase marks, lowercase
+    #     -- marks, and bookmarks.
+    #     -- can be either a table with all/none of the keys, or a single number, in which case
+    #     -- the priority applies to all marks.
+    #     -- default 10.
+    #     sign_priority = { lower=10, upper=15, builtin=8, bookmark=20 },
+    #     -- marks.nvim allows you to configure up to 10 bookmark groups, each with its own
+    #     -- sign/virttext. Bookmarks can be used to group together positions and quickly move
+    #     -- across multiple buffers. default sign is '!@#$%^&*()' (from 0 to 9), and
+    #     -- default virt_text is "".
+    #     bookmark_0 = {
+    #       sign = "⚑",
+    #       virt_text = "hello world"
+    #     },
+    #     mappings = {}
     # }
+    # '';
+    # })
+
     vim-lion # Use with gl/L<text object><character to align to 
+
 	(luaPlugin {
 	 plugin = nvim-spectre;
 	 config = ''
-	 -- nnoremap ( "n", "<leader>S",  function() require('spectre').open() end )
+	  -- nnoremap ( "n", "<leader>S",  function() require('spectre').open() end )
 	 '';
-
-
    })
+
 	# (luaPlugin {
       # plugin = nvim-gps;
 	#   config = ''
