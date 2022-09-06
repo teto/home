e44d0a9945aa97ba440d87a5b343f3b9d52e5eb8
Matthieu Coudron <mcoudron@hotmail.com>
2022-09-06 23:35:21 +0200

    restored marks

--- hm/profiles/neovim.nix
+++ hm/profiles/neovim.nix
@@ -62,7 +62,6 @@ let
 
   vimlRcBlocks = { };
 
-
   myVimPluginsOverlay = pkgs.callPackage ../../nixpkgs/overlays/vim-plugins/generated.nix {
     inherit (pkgs.vimUtils) buildVimPluginFrom2Nix;
   };
@@ -281,40 +280,40 @@ let
     #   '';
     # })
 
-    # (luaPlugin {
-    #   plugin = marks-nvim;
-    #   config = ''
-    #   require'marks'.setup {
-    #     -- whether to map keybinds or not. default true
-    #     default_mappings = true,
-    #     -- which builtin marks to show. default {} but available:  ".", "<", ">", "^"
-    #     builtin_marks = {},
-    #     -- whether movements cycle back to the beginning/end of buffer. default true
-    #     cyclic = true,
-    #     -- whether the shada file is updated after modifying uppercase marks. default false
-    #     force_write_shada = false,
-    #     -- how often (in ms) to redraw signs/recompute mark positions.
-    #     -- higher values will have better performance but may cause visual lag,
-    #     -- while lower values may cause performance penalties. default 150.
-    #     refresh_interval = 250,
-    #     -- sign priorities for each type of mark - builtin marks, uppercase marks, lowercase
-    #     -- marks, and bookmarks.
-    #     -- can be either a table with all/none of the keys, or a single number, in which case
-    #     -- the priority applies to all marks.
-    #     -- default 10.
-    #     sign_priority = { lower=10, upper=15, builtin=8, bookmark=20 },
-    #     -- marks.nvim allows you to up to 10 bookmark groups, each with its own
-    #     -- sign/virttext. Bookmarks can be used to group together positions and quickly move
-    #     -- across multiple buffers. default sign is '!@#$%^&*()' (from 0 to 9), and
-    #     -- default virt_text is "".
-    #     bookmark_0 = {
-    #       sign = "⚑",
-    #       virt_text = "hello world"
-    #     },
-    #     mappings = {}
-    # }
-    # '';
-    # })
+    (luaPlugin {
+      plugin = marks-nvim;
+      config = ''
+      require'marks'.setup {
+        -- whether to map keybinds or not. default true
+        default_mappings = true,
+        -- which builtin marks to show. default {} but available:  ".", "<", ">", "^"
+        builtin_marks = {},
+        -- whether movements cycle back to the beginning/end of buffer. default true
+        cyclic = true,
+        -- whether the shada file is updated after modifying uppercase marks. default false
+        force_write_shada = false,
+        -- how often (in ms) to redraw signs/recompute mark positions.
+        -- higher values will have better performance but may cause visual lag,
+        -- while lower values may cause performance penalties. default 150.
+        refresh_interval = 250,
+        -- sign priorities for each type of mark - builtin marks, uppercase marks, lowercase
+        -- marks, and bookmarks.
+        -- can be either a table with all/none of the keys, or a single number, in which case
+        -- the priority applies to all marks.
+        -- default 10.
+        sign_priority = { lower=10, upper=15, builtin=8, bookmark=20 },
+        -- marks.nvim allows you to up to 10 bookmark groups, each with its own
+        -- sign/virttext. Bookmarks can be used to group together positions and quickly move
+        -- across multiple buffers. default sign is '!@#$%^&*()' (from 0 to 9), and
+        -- default virt_text is "".
+        bookmark_0 = {
+          sign = "⚑",
+          virt_text = "hello world"
+        },
+        mappings = {}
+    }
+    '';
+    })
 
     vim-lion # Use with gl/L<text object><character to align to 
 	moonscript-vim
--- nixpkgs/notmuch-extract-patch.nix
+++ nixpkgs/notmuch-extract-patch.nix
@@ -1,5 +1,4 @@
-{ 
-stdenv
+{ stdenv
 , notmuch
 , buildPythonApplication
 , fetchFromGitHub
