c83767968cd5d3e19318be1d1e131b697adf19aa
Matthieu Coudron <mcoudron@hotmail.com>
2022-10-24 20:37:02 +0200

    disabled noice/papis

--- config/nvim/lua/init-manual.lua
+++ config/nvim/lua/init-manual.lua
@@ -254,31 +254,30 @@ use 'bogado/file-line' -- to open a file at a specific line
 -- use 'chrisbra/NrrwRgn' -- to help with multi-ft files
 use 'chrisbra/vim-diff-enhanced' --
 
-use({
--- 'jghauser/papis.nvim'
-  "jghauser/papis.nvim",
-  after = { "telescope.nvim", "nvim-cmp" },
-  requires = {
-    "kkharji/sqlite.lua",
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    "nvim-treesitter/nvim-treesitter",
-  },
-  rocks = {
-    {
-      "lyaml"
-      -- If using macOS or Linux, you may need to install the `libyaml` package.
-      -- If you install libyaml with homebrew you will need to set the YAML_DIR
-      -- to the location of the homebrew installation of libyaml e.g.
-      -- env = { YAML_DIR = '/opt/homebrew/Cellar/libyaml/0.2.5/' },
-    }
-  },
-  config = function()
-    require("papis").setup(
-    -- Your configuration goes here
-    )
-  end,
-})
+-- use({
+--   "jghauser/papis.nvim",
+--   after = { "telescope.nvim", "nvim-cmp" },
+--   requires = {
+--     "kkharji/sqlite.lua",
+--     "nvim-lua/plenary.nvim",
+--     "MunifTanjim/nui.nvim",
+--     "nvim-treesitter/nvim-treesitter",
+--   },
+--   rocks = {
+--     {
+--       "lyaml"
+--       -- If using macOS or Linux, you may need to install the `libyaml` package.
+--       -- If you install libyaml with homebrew you will need to set the YAML_DIR
+--       -- to the location of the homebrew installation of libyaml e.g.
+--       -- env = { YAML_DIR = '/opt/homebrew/Cellar/libyaml/0.2.5/' },
+--     }
+--   },
+--   config = function()
+--     require("papis").setup(
+--     -- Your configuration goes here
+--     )
+--   end,
+-- })
 -- use { 'ldelossa/gh.nvim',
 --     requires = { { 'ldelossa/litee.nvim' } },
 -- 	config = function ()
@@ -339,116 +338,120 @@ use {
   'AckslD/nvim-FeMaco.lua',
   config = 'require("femaco").setup()',
 }
-use { 'folke/noice.nvim',
-  event = "VimEnter",
-  requires = { "rcarriga/nvim-notify" },
-  config = function()
-	local fakeColor = { fg='#000000', bg='#00FF00' }
-	vim.api.nvim_set_hl(0, 'NoiceCmdlinePopupBorder', fakeColor)
-	vim.api.nvim_set_hl(0, 'NoiceCmdlinePopupBorderCmdline', fakeColor)
+-- use { 'folke/noice.nvim',
+--   event = "VimEnter",
+--   requires = { "rcarriga/nvim-notify" },
+--   config = function()
+-- 	local fakeColor = { fg='#000000', bg='#00FF00' }
+-- 	vim.api.nvim_set_hl(0, 'NoiceCmdlinePopupBorder', fakeColor)
+-- 	vim.api.nvim_set_hl(0, 'NoiceCmdlinePopupBorderCmdline', fakeColor)
 
-	-- https://github.com/folke/noice.nvim/wiki/Configuration-Recipes#show-recording-messages
-    require("noice").setup({
-      cmdline = {
-        view = "cmdline_popup", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
-        opts = { buf_options = { filetype = "vim" } }, -- enable syntax highlighting in the cmdline
-        icons = {
-          ["/"] = { icon = " ", hl_group = "Normal" },
-          ["?"] = { icon = " ", hl_group = "DiagnosticWarn" },
-          [":"] = { icon = " ", hl_group = "DiagnosticInfo", firstc = false },
-        },
-      },
-	  lsp_progress ={
-		  enabled = true
-	  },
-	  popupmenu = {
-		  enabled = false;
-	  },
-      history = {
-        -- options for the message history that you get with `:Noice`
-        view = "split",
-        opts = { enter = true },
-        filter = { event = "msg_show", ["not"] = { kind = { "search_count", "echo" } } },
-      },
-	  -- how frequently does Noice need to check for ui updates? This has no effect when in blocking mode.
-      throttle = 1000 / 30,
-      views = {
-        -- cmdline = {
-		-- }
-		-- @see the section on views below
-		cmdline_popup = {
-			border = {
-				-- style = "none",
-				padding = { 2, 3 },
-			},
-        -- filter_options = {},
-			win_options = {
-				winhighlight = {
-					-- Normal = "NormalFloat",
-					-- FloatBorder = "NormalFloat",
-					-- Normal = "NoicePopupmenu", -- change to NormalFloat to make it look like other floats
-					-- FloatBorder = "NoicePopupmenuBorder", -- border highlight
-					-- CursorLine = "NoicePopupmenuSelected", -- used for highlighting the selected item
-					-- PmenuMatch = "NoicePopupmenuMatch", -- used to highlight the part of the item that matches the input
-				},
-			},
-			position = {
-				-- row = 5,
-				col = "50%",
-			},
-			size = {
-				width = 210,
-				-- height = "auto",
-				height = 3
-			},
-		},
-		popupmenu = {
-			relative = "editor",
-			position = {
-			row = 8,
-			col = "50%",
-			},
-			size = {
-			width = 60,
-			height = 10,
-			},
-			border = {
-			style = "rounded",
-			padding = { 0, 1 },
-			},
-			win_options = {
-			winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
-			},
-		},
+-- 	-- https://github.com/folke/noice.nvim/wiki/Configuration-Recipes#show-recording-messages
+--     require("noice").setup({
+--       cmdline = {
+--         view = "cmdline_popup", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
+--         opts = {
+-- 			buf_options = {
+-- 				-- filetype = "vim"
+-- 			}
+-- 		}, -- enable syntax highlighting in the cmdline
+--         icons = {
+--           ["/"] = { icon = " ", hl_group = "Normal" },
+--           ["?"] = { icon = " ", hl_group = "DiagnosticWarn" },
+--           [":"] = { icon = " ", hl_group = "DiagnosticInfo", firstc = false },
+--         },
+--       },
+-- 	  lsp_progress ={
+-- 		  enabled = true
+-- 	  },
+-- 	  popupmenu = {
+-- 		  enabled = false;
+-- 	  },
+--       history = {
+--         -- options for the message history that you get with `:Noice`
+--         view = "split",
+--         opts = { enter = true },
+--         filter = { event = "msg_show", ["not"] = { kind = { "search_count", "echo" } } },
+--       },
+-- 	  -- how frequently does Noice need to check for ui updates? This has no effect when in blocking mode.
+--       throttle = 1000 / 30,
+--       views = {
+--         -- cmdline = {
+-- 		-- }
+-- 		-- @see the section on views below
+-- 		cmdline_popup = {
+-- 			border = {
+-- 				-- style = "none",
+-- 				padding = { 2, 3 },
+-- 			},
+--         -- filter_options = {},
+-- 			win_options = {
+-- 				winhighlight = {
+-- 					-- Normal = "NormalFloat",
+-- 					-- FloatBorder = "NormalFloat",
+-- 					-- Normal = "NoicePopupmenu", -- change to NormalFloat to make it look like other floats
+-- 					-- FloatBorder = "NoicePopupmenuBorder", -- border highlight
+-- 					-- CursorLine = "NoicePopupmenuSelected", -- used for highlighting the selected item
+-- 					-- PmenuMatch = "NoicePopupmenuMatch", -- used to highlight the part of the item that matches the input
+-- 				},
+-- 			},
+-- 			position = {
+-- 				-- row = 5,
+-- 				col = "50%",
+-- 			},
+-- 			size = {
+-- 				width = 210,
+-- 				-- height = "auto",
+-- 				height = 3
+-- 			},
+-- 		},
+-- 		popupmenu = {
+-- 			relative = "editor",
+-- 			position = {
+-- 			row = 8,
+-- 			col = "50%",
+-- 			},
+-- 			size = {
+-- 			width = 60,
+-- 			height = 10,
+-- 			},
+-- 			border = {
+-- 			style = "rounded",
+-- 			padding = { 0, 1 },
+-- 			},
+-- 			win_options = {
+-- 			winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+-- 			},
+-- 		},
 
-	  },
-      routes = {
-		-- skip search_count messages instead of showing them as virtual text
-          -- filter = { event = "msg_show", kind = "search_count" },
-          -- opts = { skip = true },
-      { -- shows @Recording message
-        view = "notify",
-        filter = { event = "msg_showmode" },
-      },
-      {
-        filter = {
-          event = "cmdline",
-          find = "^%s*[/?]",
-        },
-        view = "cmdline",
-      },
-	  {
-        filter = {
-          event = "msg_show",
-          kind = "",
-          find = "written",
-        },
-        opts = { skip = true },
-      },
-	  }, -- @see the section on routes below
-  })
-  end,
-}
+-- 	  },
+--       routes = {
+-- 		-- skip search_count messages instead of showing them as virtual text
+--           -- filter = { event = "msg_show", kind = "search_count" },
+--           -- opts = { skip = true },
+--       { -- shows @Recording message
+--         view = "notify",
+--         filter = { event = "msg_showmode" },
+--       },
+--       {
+--         filter = {
+--           event = "cmdline",
+--           find = "^%s*[/?]",
+--         },
+--         view = "cmdline",
+--       },
+-- 	  {
+--         filter = {
+--           event = "msg_show",
+--           kind = "",
+--           find = "written",
+--         },
+--         opts = { skip = true },
+--       },
+-- 	  }, -- @see the section on routes below
+--   })
+--   end,
+-- }
 use { 'seandewar/nvimesweeper', opt = true }
 use { 'voldikss/vim-translator', opt = true }
 use 'calvinchengx/vim-aftercolors' -- load after/colors
