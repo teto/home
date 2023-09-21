8e534c9669364590d2d4a7396b0fa72100682525
Matthieu Coudron <886074+teto@users.noreply.github.com>
2023-09-22 00:12:42 +0200

    added immich

--- nixos/profiles/immich.nix
+++ nixos/profiles/immich.nix
@@ -5,13 +5,25 @@ let
 
   # used to setup sops at the bottom of the file
   nextcloudAdminPasswordSopsPath = "nextcloud/adminPassword";
+
+  typesenseApiKeyFile = pkgs.writeText "typesense-api-key" "12318551487654187654";
+
 in
 {
  services.immich = {
-  enable = true;
+  enable = false;
   # python3.11-insightface-0.7.3
+      server.typesense.apiKeyFile = typesenseApiKeyFile;
 
  };
+
+    services.typesense = {
+      enable = true;
+      # In a real setup you should generate an api key for immich
+      # and not use the admin key!
+      apiKeyFile = typesenseApiKeyFile;
+      settings.server.api-address = "127.0.0.1";
+    };
 
 
 
