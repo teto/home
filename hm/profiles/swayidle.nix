1898d3bfbae97efe29e52419fe7265001561342d
Matthieu Coudron <886074+teto@users.noreply.github.com>
2023-11-15 19:06:56 +0100

    swayidle adjustements

--- hm/profiles/swayidle.nix
+++ hm/profiles/swayidle.nix
@@ -7,11 +7,5 @@
       { event = "before-sleep"; command = "swaylock"; }
       { event = "lock"; command = "lock"; }
     ];
-
-    # timeout <timeout> <timeout command> [resume <resume command>]
-    # Execute timeout command if there is no activity for <timeout> seconds.
-    timeouts = [
-       { timeout = 60; command = "${pkgs.swaylock}/bin/swaylock -fF"; }
-    ];
   };
 }
