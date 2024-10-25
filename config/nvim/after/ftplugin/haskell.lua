36de606358bf1fc85f67f5253cd07c3b9f9d8202
Matthieu C. <886074+teto@users.noreply.github.com>
2024-10-25 13:21:41 +0200

    adding process-compose settings.yml + postgre on laptop

---
+++ config/nvim/after/ftplugin/haskell.lua
@@ -0,0 +1,28 @@
+-- described in https://github.com/MrcJkb/haskell-tools.nvim#quick-setup
+local ht = require('haskell-tools')
+local buffer = vim.api.nvim_get_current_buf()
+local def_opts = { noremap = true, silent = true }
+--
+-- -- print(vim.inspect(ht))
+-- -- ht.start_or_attach(opts)
+-- -- ht.setup(opts)
+--
+-- Suggested keymaps that do not depend on haskell-language-server:
+local bufnr = vim.api.nvim_get_current_buf()
+-- set buffer = bufnr in ftplugin/haskell.lua
+local map_opts = { noremap = true, silent = true, buffer = bufnr }
+--
+-- -- Toggle a GHCi repl for the current package
+-- vim.keymap.set('n', '<leader>rr', ht.repl.toggle, map_opts)
+-- -- Toggle a GHCi repl for the current buffer
+-- vim.keymap.set('n', '<leader>rf', function()
+--     ht.repl.toggle(vim.api.nvim_buf_get_name(0))
+-- end, def_opts)
+-- vim.keymap.set('n', '<leader>rq', ht.repl.quit, map_opts)
+--
+-- vim.keymap.set('n', '<space>hs', function()
+--     require('haskell-tools').hoogle.hoogle_signature()
+-- end)
+-- Detect nvim-dap launch configurations
+-- (requires nvim-dap and haskell-debug-adapter)
+-- ht.dap.discover_configurations(bufnr)
---
+++ hosts/laptop/services/postgresql.nix
@@ -0,0 +1,42 @@
+{
+  config,
+  flakeInputs,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  # _imports = [
+  #   ../../nixos/profiles/postgresql.nix
+  # ];
+
+  enable = true;
+  enableTCPIP = true; # if false, use TCP via localhost only or via socket
+
+  # port par defaut est 5432
+  # port = 5555;
+
+  # should be a string
+  # initialPasswordFile
+
+  # initialScript = pkgs.writeText "init-sql-script" ''
+  #   alter user postgres with password '${postgresPassword}';
+  # '';
+
+  # ensureDatabases = [ "core" ];
+  # Les mots de passe PostgreSQL sont distincts des mots de passe du système d'exploitation. Le mot de passe de chaque utilisateur est enregistré dans le catalogue système pg_authid. Ils peuvent être gérés avec les commandes SQL CREATE ROLE et ALTER ROLE. Ainsi, par exemple, CREATE ROLE foo WITH LOGIN PASSWORD 'secret';
+  # pg_hba.conf is generated through authentication bit
+  # sudo cat /var/lib/postgresql/14/pg_hba.conf for a primer
+  # "local   all             all                                     md5
+  # 'password' is in clear text
+  # use password instead of trust ?
+  authentication = ''
+    # TYPE  DATABASE        USER            ADDRESS                 METHOD
+    host    all             all             samenet            trust
+    local   all             all                                peer
+  '';
+
+  # of the form map-name system-username database-username
+  # identMap = "desktop-map teto postgres";
+
+}
