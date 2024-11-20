1ee9a3499f02907805eb6c2061441af40aa962bb
Matthieu C. <886074+teto@users.noreply.github.com>
2024-11-20 09:56:55 +0000

    disabled cgroups + broken fcitx5-mozc

--- hm/profiles/fcitx.nix
+++ hm/profiles/fcitx.nix
@@ -5,7 +5,9 @@
     enabled = null;
     # "fcitx5";
     # fcitx5.addons = with pkgs.fcitx-engines;  [ mozc ];
-    fcitx5.addons = with pkgs; [ fcitx5-mozc ];
+    fcitx5.addons = with pkgs; [ 
+      # fcitx5-mozc
+    ];
     # fcitx5.waylandFrontend = true;
 
     inputs = {
