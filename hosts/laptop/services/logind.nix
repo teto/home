e33fee6aa35d72e3d91894ee996babf5f1bfe614
Matthieu C. <886074+teto@users.noreply.github.com>
2025-08-30 17:28:23 +0200

    updated typescript.nvim to use fork

--- hosts/laptop/services/logind.nix
+++ hosts/laptop/services/logind.nix
@@ -1,16 +1,17 @@
 {
-  #   # see https://bbs.archlinux.org/viewtopic.php?id=225977 for problems with LID
-  #   # lidSwitch = "ignore";
-  lidSwitch = "suspend";
-  #   lidSwitchDocked = "suspend";
-  lidSwitchExternalPower = "ignore";
 
   # see https://nixos.org/nix-dev/2015-July/017657.html for problems
   # with /run/user/1000 size
   # size of tmpfs used by nix builds
   # usually
   # see cat /etc/systemd/logind.conf
-  extraConfig = ''
-    RuntimeDirectorySize=6G
-  '';
+  settings.Login = {
+    # conflicted with another value
+    #   RuntimeDirectorySize="6G";
+
+    # see https://bbs.archlinux.org/viewtopic.php?id=225977 for problems with LID
+    # lidSwitch = "ignore";
+    HandleLidSwitchExternalPower = "ignore";
+    HandleLidSwitch = "suspend";
+  };
 }
