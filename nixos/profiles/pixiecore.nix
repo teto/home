636177c24b5e417714340d5587d6c58138a8335b
Matthieu C. <886074+teto@users.noreply.github.com>
2025-10-25 17:09:13 +0200

    added neptune + pixiecore config to act as a supplementrary dhcp (what i want in 99% of cases

--- nixos/profiles/pixiecore.nix
+++ nixos/profiles/pixiecore.nix
@@ -14,10 +14,16 @@ in
 
     # extraArguments = ;
     # listen
+    # cmdLine = ;
 
+     dhcpNoBind = true; # Use existing DHCP server.
+
+    quick = "ubuntu";
     openFirewall = true;
-    # mode = "boot";
-    # kernel =
+
+    # mode = "boot"; / "quick"
+
+    # https://carlosvaz.com/posts/ipxe-booting-with-nixos/
     kernel = "https://boot.netboot.xyz";
     # kernel = "${netboot}/bzImage";
     # package =  pkgs.linux_mptcp_trunk_raw;
