e2700dc57c28265e989f7ca95bbb93daefaa34a4
Matthieu C. <886074+teto@users.noreply.github.com>
2025-12-12 22:07:13 +0100

    embedding pass-fzf copied from https://github.com/ficoos/pass-fzf/blob/master/fzf.bash
    restored codecompanion + claude

--- config/nvim/lua/plugins/codecompanion.lua
+++ config/nvim/lua/plugins/codecompanion.lua
@@ -1,14 +1,31 @@
 require('codecompanion').setup({
 
-    secret = { 'bash', '-c', 'cat $XDG_CONFIG_HOME/sops-nix/secrets/OPENAI_API_KEY_NOVA' },
-
-    adapters = {
-        openai = function()
-            return require('codecompanion.adapters').extend('openai', {
-                env = {
-                    api_key = "cmd:bash -c 'cat $XDG_CONFIG_HOME/sops-nix/secrets/OPENAI_API_KEY_NOVA'",
-                },
-            })
-        end,
-    },
-})
+    secret = { 'bash', '-c', 'cat $XDG_CONFIG_HOME/sops-nix/secrets/claude_api_key' },
+   strategies = {
+	 chat = {
+	   adapter = "anthropic",
+	   model = "claude-sonnet-4-20250514"
+	 },
+	 inline = {
+	   adapter = "anthropic",
+	 },
+	},
+	 adapters = {
+	  http = {
+		 openai = function()
+			 return require('codecompanion.adapters').extend('openai', {
+				 env = {
+					 api_key = "cmd:bash -c 'cat $XDG_CONFIG_HOME/sops-nix/secrets/claude_api_key'",
+				 },
+			 })
+		 end,
+      anthropic = function()
+        return require("codecompanion.adapters").extend("anthropic", {
+          env = {
+            api_key =  'cmd:cat /home/teto/.config/sops-nix/secrets/claude_api_key',
+          },
+        })
+      end,
+	 },
+	 },
+ })
