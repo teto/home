d798e9c8b4678b4359863efb08aad535269ccac3
Matthieu C. <886074+teto@users.noreply.github.com>
2026-08-27 05:17:20 +0200

    bumped rocks.lock + abbr
    improving fish abbreviations

--- hosts/tatooine/networking/default.nix
+++ hosts/tatooine/networking/default.nix
@@ -12,10 +12,14 @@
 
   hosts = {
     # a test, better would be to have nginx recognize another thing
+    # todo use from lib
     "10.100.0.1" = [ "neotokyo.local" ];
   };
 
-  firewall.allowedTCPPorts = lib.optional config.services.rmfakecloud.enable config.services.rmfakecloud.port;
+  firewall.allowedTCPPorts = lib.optional config.services.rmfakecloud.enable config.services.rmfakecloud.port ++ [
+    10301 # whisper service
+    10400 # openwakeword
+  ];
 
   firewall.allowedUDPPorts = [
     51820 # wireguard
